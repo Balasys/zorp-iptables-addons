251d5ced7031f90e2c2f20ae26cf388111ae2a57
Szilárd Pfeiffer <pfeiffer.szilard@balabit.hu>
2015-01-08 09:28:53 +0100

    service match: Modernized code according to the upstream matches.
    References: jira ZORP-208

--- extensions/libxt_service.c
+++ extensions/libxt_service.c
@@ -30,16 +30,27 @@ static struct option service_opts[] = {
 };
 
 enum {
-	F_NAME = 1 << 0,
-	F_TYPE = 1 << 1,
+	O_NAME		= 0,
+	O_TYPE		= 1,
+	F_NAME		= 1 << O_NAME,
+	F_TYPE		= 1 << O_TYPE,
 };
+
+#define s struct xt_service_info
+static struct xt_option_entry service_opts_v2[] = {
+	{ .name = "service-name",	.id = O_NAME,		.type = XTTYPE_STRING,
+	  .flags = XTOPT_PUT, XTOPT_POINTER(s, name), .size = XT_SERVICE_NAME_LENGTH },
+	{ .name = "service-type",	.id = O_TYPE,		.type = XTTYPE_STRING },
+	{ .name = NULL }
+};
+#undef s
 
 static int
 service_parse(int c, char **argv, int invert, unsigned int *flags,
 	      const void *entry, struct xt_entry_match **match)
 {
-	struct ipt_service_info *info =
-			(struct ipt_service_info *) (*match)->data;
+	struct xt_service_info *info =
+			(struct xt_service_info *) (*match)->data;
 
 	switch (c) {
 	case '1': /* service-name */
@@ -54,12 +65,12 @@ service_parse(int c, char **argv, int invert, unsigned int *flags,
 				   "a service name\n");
 
 		strncpy((char *)info->name, optarg, sizeof(info->name));
-		info->name[IPT_SERVICE_NAME_LENGTH] = '\0';
+		info->name[XT_SERVICE_NAME_LENGTH] = '\0';
 
 		if (strcmp(optarg, "*") == 0)
-			info->name_match = IPT_SERVICE_NAME_WILDCARD;
+			info->name_match = XT_SERVICE_NAME_WILDCARD;
 		else
-			info->name_match = IPT_SERVICE_NAME_MATCH;
+			info->name_match = XT_SERVICE_NAME_MATCH;
 
 		*flags |= F_NAME;
 		break;
@@ -78,9 +89,9 @@ service_parse(int c, char **argv, int invert, unsigned int *flags,
 				   "by a valid service type\n");
 
 		if (strcmp(optarg, "forward") == 0)
-			info->type = IPT_SERVICE_TYPE_FORWARD;
+			info->type = XT_SERVICE_TYPE_FORWARD;
 		else
-			info->type = IPT_SERVICE_TYPE_PROXY;
+			info->type = XT_SERVICE_TYPE_PROXY;
 
 		*flags |= F_TYPE;
 		break;
@@ -93,6 +104,35 @@ service_parse(int c, char **argv, int invert, unsigned int *flags,
 }
 
 static void
+service_parse_v2(struct xt_option_call *cb)
+{
+	struct xt_service_info_v2 *info = cb->data;
+
+	xtables_option_parse(cb);
+
+	switch (cb->entry->id) {
+	case O_NAME:
+		if (strcmp(cb->arg, "*") == 0)
+			info->name_match = XT_SERVICE_NAME_WILDCARD;
+		else
+			info->name_match = XT_SERVICE_NAME_MATCH;
+		break;
+
+	case O_TYPE:
+		if (strcmp(optarg, "forward") == 0)
+			info->type = XT_SERVICE_TYPE_FORWARD;
+		else if (strcmp(optarg, "proxy") != 0)
+			info->type = XT_SERVICE_TYPE_PROXY;
+		else
+			xtables_error(PARAMETER_PROBLEM,
+				   "`--service-type' must be accompanied "
+				   "by a valid service type\n");
+		break;
+	}
+}
+
+
+static void
 service_final_check(unsigned int flags)
 {
 	if (!(flags & (F_NAME | F_TYPE)))
@@ -102,47 +142,71 @@ service_final_check(unsigned int flags)
 }
 
 static void
+service_final_check_v2(struct xt_fcheck_call *cb)
+{
+	service_final_check(cb->xflags);
+}
+
+static void
 service_print(const void *ip, const struct xt_entry_match *match, int numeric)
 {
-	struct ipt_service_info *info = (struct ipt_service_info *) match->data;
+	struct xt_service_info *info = (struct xt_service_info *) match->data;
 
 	fputs("service ", stdout);
-	if (info->name_match != IPT_SERVICE_NAME_ANY)
+	if (info->name_match != XT_SERVICE_NAME_ANY)
 		printf(" name %s", info->name);
-	if (info->type != IPT_SERVICE_TYPE_ANY)
+	if (info->type != XT_SERVICE_TYPE_ANY)
 		printf(" type %s",
-		       (info->type == IPT_SERVICE_TYPE_PROXY) ? "proxy "
+		       (info->type == XT_SERVICE_TYPE_PROXY) ? "proxy "
 							      : "forward ");
 }
 
 static void
 service_save(const void *ip, const struct xt_entry_match *match)
 {
-	struct ipt_service_info *info = (struct ipt_service_info *)match->data;
+	struct xt_service_info *info = (struct xt_service_info *)match->data;
 
-	if (info->name_match != IPT_SERVICE_NAME_ANY)
+	if (info->name_match != XT_SERVICE_NAME_ANY)
 		printf(" --service-name %s", info->name);
-	if (info->type != IPT_SERVICE_TYPE_ANY)
+	if (info->type != XT_SERVICE_TYPE_ANY)
 		printf(" --service-type %s",
-		       (info->type == IPT_SERVICE_TYPE_PROXY) ? "proxy "
+		       (info->type == XT_SERVICE_TYPE_PROXY) ? "proxy "
 							      : "forward ");
 }
 
-static struct xtables_match service = {
-	.name		= "service",
-	.family		= NFPROTO_UNSPEC,
-	.version	= XTABLES_VERSION,
-	.size		= XT_ALIGN(sizeof(struct ipt_service_info)),
-	.userspacesize	= XT_ALIGN(sizeof(struct ipt_service_info)),
-	.help		= service_help,
+static struct xtables_match service_match = {
+ 	.name		= "service",
+ 	.family		= NFPROTO_UNSPEC,
+	.revision	= 1,
+ 	.version	= XTABLES_VERSION,
+	.size		= XT_ALIGN(sizeof(struct xt_service_info)),
+	.userspacesize	= XT_ALIGN(sizeof(struct xt_service_info)),
+ 	.help		= service_help,
 	.parse		= service_parse,
 	.final_check	= service_final_check,
-	.print		= service_print,
-	.save		= service_save,
+ 	.print		= service_print,
+ 	.save		= service_save,
 	.extra_opts	= service_opts
 };
 
+static struct xtables_match service_match_v2 = {
+	.name		= "service",
+	.family		= NFPROTO_UNSPEC,
+	.revision	= 2,
+	.version	= XTABLES_VERSION,
+	.size		= XT_ALIGN(sizeof(struct xt_service_info_v2)),
+	.userspacesize	= XT_ALIGN(sizeof(struct xt_service_info_v2)),
+	.help		= service_help,
+	.x6_parse	= service_parse_v2,
+	.x6_fcheck	= service_final_check_v2,
+	.print		= service_print,
+	.save		= service_save,
+	.x6_options	= service_opts_v2
+};
+
+
 void _init(void)
 {
-	xtables_register_match(&service);
+	xtables_register_match(&service_match);
+	xtables_register_match(&service_match_v2);
 }
--- extensions/xt_service.h
+++ extensions/xt_service.h
@@ -2,26 +2,36 @@
 #define _XT_SERVICE_H
 
 enum {
-	IPT_SERVICE_TYPE_ANY = 0,
-	IPT_SERVICE_TYPE_PROXY,
-	IPT_SERVICE_TYPE_FORWARD,
+	XT_SERVICE_TYPE_ANY = 0,
+	XT_SERVICE_TYPE_PROXY,
+	XT_SERVICE_TYPE_FORWARD,
 };
 
 enum {
-	IPT_SERVICE_NAME_ANY = 0,
-	IPT_SERVICE_NAME_WILDCARD,
-	IPT_SERVICE_NAME_MATCH,
+	XT_SERVICE_NAME_ANY = 0,
+	XT_SERVICE_NAME_WILDCARD,
+	XT_SERVICE_NAME_MATCH,
 };
 
-#define IPT_SERVICE_NAME_LENGTH 117
+#define XT_SERVICE_NAME_LENGTH 117
 
-struct ipt_service_info {
+struct xt_service_info {
 	u_int8_t type;
 	u_int8_t name_match;
-	unsigned char name[IPT_SERVICE_NAME_LENGTH + 1];
+	unsigned char name[XT_SERVICE_NAME_LENGTH + 1];
 
 	unsigned int generation;
 	unsigned int service_id;
 };
+
+struct xt_service_info_v2 {
+	u_int8_t type;
+	u_int8_t name_match;
+	unsigned char name[XT_SERVICE_NAME_LENGTH + 1];
+
+	unsigned int generation;
+	unsigned int service_id;
+};
+
 
 #endif
